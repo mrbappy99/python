5d02b67cef0a126a19310faac93502895c259539
Motiur Rahman Bappy <71618703+mrbappy99@users.noreply.github.com>
2022-07-25 11:21:12 +0600

    Create a Python program to enter two numbers and perform all arithmetic operations.py

---
+++ a Python program to enter two numbers and perform all arithmetic operations.py
@@ -0,0 +1,15 @@
+num1 =int( input("First number : "))
+num2 =int( input("Second number: "))
+
+Sum = num1+ num2
+Difference = num1 - num2
+Product = num1 * num2
+Quotient = num1 // num2
+Modulus = num1 % num2
+
+
+print("Sum =", Sum, "\n"
+"Difference =" ,Difference, "\n" 
+"Product =", Product , "\n"
+"Quotient =" , Quotient , "\n"
+"Modulus =",Modulus)
