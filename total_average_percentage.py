9f8ea99a846ddda8b68e192dc4ce09b5cc08e68c
Motiur Rahman Bappy <71618703+mrbappy99@users.noreply.github.com>
2022-07-25 21:25:05 +0600

    Create total_average_percentage.py

---
+++ total_average_percentage.py
@@ -0,0 +1,15 @@
+# Total, average, 
+
+num1 = int(input("1st number : "))
+num2 = int(input("2nd number : "))
+num3 = int(input("3rd number : "))
+num4 = int(input("4rth number : "))
+num5 = int(input("5th number : "))
+
+totl = (num1 + num2 + num3 + num4 + num5)
+averg = (num1 + num2 + num3 + num4 + num5) / 5
+prcntg = (totl / 500) * 100
+
+print("The total is ", totl)
+print("The average is ", averg)
+print("The percentage is ", prcntg)
