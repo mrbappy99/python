534abd185eafc4930d178e8403f4d1b4aa43751a
Motiur Rahman Bappy <71618703+mrbappy99@users.noreply.github.com>
2022-07-25 11:46:33 +0600

    Update rectangle related math.py

--- rectangle related math.py
+++ rectangle related math.py
@@ -5,20 +5,20 @@ print ("Ok, lets find perimeter of any rectangle")
 
 # Find perimeter of a rectangle 
 
-length = int(input ("length ="))
-width = int(input ("width ="))
+length = int(input ("length (in meter) ="))
+width = int(input ("width (in meter) ="))
 
 result = 2 * (length + width)
 
-print( "The perimeter of that rectangle is" , result)
+print( "The perimeter of that rectangle is" , result, "meter.")
 
 print("Now find area of a rectangle")
 
 # Find area of a rectangle 
 
-length = int(input ("length ="))
-width = int(input ("width ="))
+length = int(input ("length (in meter) ="))
+width = int(input ("width (in meter) ="))
 
 result = (length * width)
 
-print( "The area of that rectangle is" , result)
+print( "The area of that rectangle is" , result, "sq. meter")
