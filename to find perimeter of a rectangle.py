1baab9a4b41831361f71cc8d241b10c6bf47d0b7
Motiur Rahman Bappy <71618703+mrbappy99@users.noreply.github.com>
2022-07-25 11:33:27 +0600

    Create to find perimeter of a rectangle.py

---
+++ to find perimeter of a rectangle.py
@@ -0,0 +1,8 @@
+# Find perimeter of a rectangle 
+
+length = int(input ("length ="))
+width = int(input ("width ="))
+
+result = 2 * (length + width)
+
+print( "The perimeter of that rectangle is" , result)
