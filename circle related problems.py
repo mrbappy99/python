ce1d3f1fbbc1b600992dd00abfba8ae421f2cd3d
Motiur Rahman Bappy <71618703+mrbappy99@users.noreply.github.com>
2022-07-25 16:01:44 +0600

    Create circle related problems.py

---
+++ circle related problems.py
@@ -0,0 +1,13 @@
+#circle releted math
+
+PI = 3.14
+
+radius = (int (input("inter the radius of the circle : ")))
+
+diameter = 2*radius
+circumference = 2*PI*radius
+area = PI*(radius*radius)
+
+print("The diameter is : ", diameter)
+print("The circumference is : ", circumference)
+print("The area is : ", area)
